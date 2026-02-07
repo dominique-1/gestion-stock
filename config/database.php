ab0559de15e5d3cc75d0bc0cd6793af3f3c214c2
Developer <dev@example.com>
2026-02-07 22:18:01 +0100

    Ajout configuration PostgreSQL

--- config/database.php
+++ config/database.php
@@ -48,6 +48,21 @@ return [
             'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
+        
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'stock'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
+        ],
     ],
     
     'migrations' => [
